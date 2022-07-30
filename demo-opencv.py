615e19fd9340fc61f68294e276469eaaa71c60e3
And4Web <anands2807@gmail.com>
2022-07-30 16:45:34 +0530

    learned some basics and now starting the main project

--- demo-opencv.py
+++ demo-opencv.py
@@ -1,6 +1,9 @@
 import cv2
 
-image = cv2.imread("sample.png")
+image = cv2.imread("setup.jpg")
+image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+image = cv2.bitwise_not(image)
+
 cv2.imshow("this is processed image", image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
